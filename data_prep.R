3fa84d9ebc7b5db17af9de1d637aaaaaf4e8480d
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2023-03-02 20:12:27 +1300

    Almost there -- need to figure out how to keep index (sample ID)

--- data_prep.R
+++ data_prep.R
@@ -1,26 +1,33 @@
 library(tidyverse)
 library(data.table)
 
+
 DATA_DIR <- "CSV files"
 
-#Read in all files
-data_filenames <- list.files(path=DATA_DIR, pattern = "*.cvs")
+file_list <- list.files(path=DATA_DIR, pattern = "*.csv", full.names =TRUE)
+data_list <- vector("list", "length" = length(file_list))
+final_data <-data_frame()
 
 
-data1 <- read.csv(file = "CSV files/V3.1-1.csv", skip = 8, header = TRUE, stringsAsFactors = FALSE)
-data1 <- data1 %>% select(Name, Peak.Height)
-data1 <- data.table::transpose(data1)
-colnames(data1) <- data1[1,]
-data1<- data1[-1, ]
+for (i in seq_along(file_list)) {
+  filename = file_list[[i]]
+  
+  # Read data in
+  data1 <- read.csv(file = filename, skip = 8, header = TRUE, stringsAsFactors = FALSE)
+  # Select name of the compound and desired value (Peak Height)
+  data1 <- data1 %>% select(Name, Peak.Height)
+  # Transpose: columns become rows
+  data1 <- data.table::transpose(data1)
+  # Set column names to be names of the compounds
+  colnames(data1) <- data1[1,]
+  data1<- data1[-1, ]
+  #Set the row name to correspond to the sample ID
+  rownames(data1)<- colnames(read.csv(file = filename, stringsAsFactors = FALSE, sep = '\t' ))
+  # Add year to data_list
+  data_list[[i]] <- data1
+  final_data <- merge(final_data, data1, all=TRUE) 
+}
 
-data2 <- read.csv(file = "CSV files/V3.1-2.csv", skip = 8, header = TRUE, stringsAsFactors = FALSE)
-data2 <- data2 %>% select(Name, Peak.Height)
-data2 <- data.table::transpose(data2)
-colnames(data2) <- data2[1,]
-data2 <- data2[-1, ]
 
-
-#Joining
-final_data <- list(data1, data2) %>% reduce(full_join)
 
 
